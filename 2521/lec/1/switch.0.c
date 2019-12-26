d6a58d064c093d73085a9822b2f4f5ae91ca2cf3
ason <ahsan.virani08@gmail.com>
2019-12-27 01:27:48 +1100

    Switch Statements

---
+++ 2521/lec/1/switch.0.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+void flagFunction(void)
+{
+	char color[5] = {'w', 'g', 'b', 'y', 'r'};
+	
+	for(int i = 0; i < 5; i++)
+	{
+		switch(color[i])
+		{
+			case 'g': puts("Your color is green"); printf("at %d\n", i); break;
+		
+			case 'w': puts("Your color is white"); printf("at %d\n", i); break;
+
+			default: puts("Something else"); printf("at %d\n", i); 
+		}
+	
+	}
+
+}
+
+int
+main(void)
+{
+
+	flagFunction();
+
+	return 0;
+
+}
+
+
