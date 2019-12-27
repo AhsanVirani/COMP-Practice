323577363f35d15af439eda993df04b8993d1aaa
ason <ahsan.virani08@gmail.com>
2019-12-28 01:27:44 +1100

    getchar

--- 2521/lec/1/getchar.0.c
+++ 2521/lec/1/getchar.0.c
@@ -5,16 +5,17 @@ main(void)
 {
 	
 	char myChar = '\0';
-
+	int nchars = 0;
 	while(myChar != EOF)
 	{
 		myChar = getchar();
-	
+		nchars++;
 		printf("%d on %c\n", myChar, myChar);
 
 	}
-
+	
 	printf("stopped intentionally\n");
+	printf("count = %d\n", nchars);
 
 	return 0;
 }
