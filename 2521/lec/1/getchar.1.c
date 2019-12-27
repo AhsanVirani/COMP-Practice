2394ee22b85958bc7b8155bddb43dd68fe9ad7f9
ason <ahsan.virani08@gmail.com>
2019-12-28 01:43:10 +1100

    getchar in loop

---
+++ 2521/lec/1/getchar.1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+int
+main(void)
+{
+	
+	char myChar = '\0';
+	int nchars = 0;
+	while((myChar = getchar()) != EOF)
+	{
+		nchars++;
+		printf("%d on %c\n", myChar, myChar);
+
+	}
+	
+	printf("stopped intentionally\n");
+	printf("count = %d\n", nchars);
+
+	return 0;
+}
