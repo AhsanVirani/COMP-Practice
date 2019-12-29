ccb406c6fe65d438e18869d91d500da79dba63dd
ason <ahsan.virani08@gmail.com>
2019-12-30 00:22:02 +1100

    woks

--- 2521/1/prac/funcPointer.0.c
+++ 2521/1/prac/funcPointer.0.c
@@ -12,17 +12,17 @@ main(void)
 	int *ibox = &a;
 
 	int (*sbox)(int);	// fbox is a pointer to a function that takes 1 parameter of type int and returns an int
-	sbox = square;
+	sbox = &square;
 
 	int (*abox)(int, int);
-	abox = add;
+	abox = &add;
 
 
-	printf("square at %p took %d and returned -> %d\n", square, a, square(a));	
+	printf("square at %p took %d and returned -> %d\n", &square, a, square(a));	
 	printf("%d\n", sbox(a));
 	printf("%d is at address %p\n", a, ibox);
-
-	printf("add at %p took %d %d and returned -> %d\n", abox, a, a, abox(a, a) );	// A function is just an address in memory that stores the derivation you do. i.e. {return x + y}
+	printf("abox variable at %p stores pointer to function add at %p\n", &abox, *(&abox));
+	printf("add at %p XX %p XX %p XX %p took %d %d and returned -> %d\n", &add, *(&abox), add, abox, a, a, abox(a, a) );	// A function is just an address in memory that stores the derivation you do. i.e. {return x + y}
 
 	printf("printf is here in memory sea -> %p\n", printf);
 
