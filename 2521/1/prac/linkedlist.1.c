f99f14bc6f5e6bcfeb5ae86e22b61d1d7c49328f
ason <ahsan.virani08@gmail.com>
2019-12-31 00:16:29 +1100

    up

---
+++ 2521/1/prac/linkedlist.1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int		data;
+	struct node *	next;
+};
+
+int
+main(void)
+{
+
+	struct node *n1 = malloc(sizeof(struct node));
+	struct node *n2 = malloc(sizeof(struct node));
+
+	(*n1).data = 5; 	(*n1).next = n2;
+	(*n2).data = 10; 	(*n2).next = NULL;
+	printf("n1 stores the address %p\n", n1);
+	printf("n2 stores the address %p\n", n2);
+
+	struct node *curr = n1;
+	while(curr != NULL)
+	{
+
+		printf("%d at address %p\n",(*curr).data, curr);
+		curr = (*curr).next;
+
+	}
+	
+	free(n1); free(n2); n1 = NULL; n2 = NULL;
+
+
+	return 0;
+
+}
