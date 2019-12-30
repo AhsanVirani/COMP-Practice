a39b5050d226ea2d6ea3c4a97ab3325f02ed120e
ason <ahsan.virani08@gmail.com>
2019-12-31 00:50:48 +1100

    typedefing

--- 2521/1/prac/linkedlist.2.c
+++ 2521/1/prac/linkedlist.2.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
-	int		data;
-	struct node *	next;
-};
+// typedefing explained
+
+typedef struct _node *node;
+typedef struct _node {
+	int	data;
+	node	next;
+} _node;
 
 int
 main(void)
 {
 
-	struct node *n1 = malloc(sizeof(struct node));
-	struct node *n2 = malloc(sizeof(*n2)); // shortcut using the definition itself
+	node n1 = malloc(sizeof(*n1));
+	node n2 = malloc(sizeof(*n2));
 
 	(*n1).data = 5; 	(*n1).next = n2;
-	(*n2).data = 10; 	(*n2).next = NULL;
+	n2->data = 10; 	(*n2).next = NULL;
 	printf("n1 stores the address %p\n", n1);
 	printf("n2 stores the address %p\n", n2);
 
-	struct node *curr = n1;
+	node curr = n1;
 	while(curr != NULL)
 	{
-
 		printf("%d at address %p\n",(*curr).data, curr);
 		curr = (*curr).next;
-
 	}
 	
 	free(n1); free(n2); n1 = NULL; n2 = NULL;
