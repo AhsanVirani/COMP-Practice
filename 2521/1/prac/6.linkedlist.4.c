4ac04d6a07e035da374efff3df304f13e0d318f0
ason <ahsan.virani08@gmail.com>
2020-01-04 01:29:39 +1100

    list

--- 2521/1/prac/6.linkedlist.4.c
+++ 2521/1/prac/6.linkedlist.4.c
@@ -12,11 +12,69 @@ typedef struct _node{
 } _node;
 
 
+node
+create_node()
+{
+	node n = malloc(sizeof((*n)));
+	if(n == NULL)
+	{
+		fprintf(stderr, "No memory to allocate\n");
+		exit(EXIT_FAILURE);
+
+	}
+	
+	(*n).data = NULL;	
+	(*n).next = NULL;
+
+	return n;
+
+}
+
+void
+show_reverse(node n)
+{
+
+	if((*n).next == NULL)
+	{
+		printf("%s ", (*n).data);
+
+	}
+
+	else{
+		show_reverse((*n).next);
+		printf("%s ", (*n).data);
+	
+	}	
+
+
+}
+
+void
+free_list(node n)
+{
+
+	if((*n).next == NULL)
+	{
+		free(n);
+	}
+	
+	else{
+
+		free_list((*n).next);
+		free(n);
+
+	}
+
+}
+
+
 int
 main(void)
 {
 
 	node n1 = malloc(sizeof((*n1)));
+	node n2 = malloc(sizeof((*n2))); //
+
 	if(n1 == NULL)	
 	{	
 		fprintf(stderr, "No memory to allocate\n");
@@ -24,14 +82,49 @@ main(void)
 	}
 
 	(*n1).data = NULL; (*n1).next = NULL;
-	// char a[5] = {'h', 'e', 'l', 'l', 'o', '\0'}; <==> 
-	char *a = "hello";
-
-	putchar(*(a+2)); putchar(a[0]); putchar(*(a+0));
 	(*n1).data = "HELLO";
-	printf("%c\n", *((*n1).data+2));
+
+	if(n2 == NULL)	//
+	{	
+		fprintf(stderr, "No memory to allocate\n"); //
+		exit(EXIT_FAILURE); //
+	}
+
+	(*n2).data = NULL; (*n2).next = NULL; //
+	(*n2).data = "GANDU";
+
+	printf("%s %s\n", (*n1).data, (*n2).data);
+
+
+	(*n1).next = n2;
+	printf("%s %s\n", (*n1).data, (*(*n1).next).data);
+
+	node n3 = create_node();
+	(*n2).next = n3;
 	
-		
+	(*n3).data = "!!";		
+	printf("%s %s%s\n", (*n1).data, (*n2).data, (*n3).data);
+
+	node c = n1;
+	while(c != NULL)
+	{
+		printf("%s ", (*c).data);
+		c = (*c).next;
+
+	}
+	printf("\n");
+
+	for(node d = n1; d != NULL; d = (*d).next)
+	{
+		printf("%s ", (*d).data);
+
+	}
+	printf("\n");
+
+	show_reverse(n1);
+	printf("\n");
+
+	free_list(n1);
 
 	return 0;
 
